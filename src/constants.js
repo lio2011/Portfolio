542390c209c46822f816d6b0db0f9d26fedd2e71
lio2011 <sshouryan@gmail.com>
2024-04-22 12:08:04 +0530

    modified text

--- src/constants.js
+++ src/constants.js
@@ -13,7 +13,7 @@ export const dialogueData = {
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. I have filed <b>2 patents</b> as a result while working at Bank of America. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1j96sDmYgXjP5O0hGc6O4gTv8E6jyOCZn/view?usp=sharing" target="_blank">Check it out?</a>
     Contact me at sshouryan@gmail.com / +91 9971830068 if you have any interesting job opportunities!`,
-  projects: ` Visit all my projects : <a href="https://github.com/lio2011" target = "_blank">here</a>
+  projects: ` Visit all my projects : <a href="https://github.com/lio2011" target = "_blank">here</a><br>
     Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
     Kaboom.js by watching some of these tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
